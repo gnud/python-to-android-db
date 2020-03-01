d08e7b48ddac3a8bcdb94fd1698281d5b0aa8807
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-03-01 21:56:44 +0100

    Removing 'mymodels' cause it's silly idea (#4)
    Bump Django to latest minor version, after Github threats

--- databuilder/models.py
+++ databuilder/models.py
@@ -1,11 +1,9 @@
 # noinspection PyUnresolvedReferences
 # We define models for in Android in this module, since we like to make models.py a generic file for any project
 from django.conf import settings
+from django.db import models
 
-from databuilder.mymodels import *
 
-
-# Write your tools models here
 class SampleTest(models.Model):
     """
     Only for unit testing
@@ -16,3 +14,5 @@ class SampleTest(models.Model):
     last_updated = models.DateTimeField(auto_now=True, editable=False)
 
     name = models.CharField(max_length=1000)
+
+# Add real models bellow this line
